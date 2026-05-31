1d6c5c9d165c984a62d4f52ed6b588d1e7eb3c2c
menene <efmarroquinr@gmail.com>
2026-05-31 16:06:51 -0600

    infra

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,7 +2,7 @@ const isLocal = window.location.hostname === 'localhost'
 
 export const API_BASE = isLocal
   ? 'http://localhost:8000'
-  : `${window.location.protocol}//api.cookielab.cc`
+  : ''
 
 export const LAB_URL = isLocal
   ? 'http://localhost:8888'
